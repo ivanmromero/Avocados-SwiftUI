e0c59784f76bccb2885e1ac6ae73d0dddbac296e
Ivan Romero <ivan_manuel_romero@hotmail.com>
2024-02-14 19:40:40 -0300

    Initial config

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Ivan Romero on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Avocados/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Avocados
+//
+//  Created by Ivan Romero on 14/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
